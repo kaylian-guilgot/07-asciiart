003fe87f06560b5ed26952d8a275915aa68d7a72
kaylian-guilgot <kaylian.guilgot@edu.esiee.fr>
2025-11-01 14:37:51 +0000

    Modification de main(), artcode_i() et artcode_r()

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+"""Programme qui encode une chaine de caractères par une liste de tuples"""
 #### Imports et définition des variables globales
 
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
 
-
 #### Fonctions secondaires
 
-
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée 
+    en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +17,21 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    c = [s[0]]
+    o = [1]
+    for k in range(1, len(s)):
+        if s[k] == s[k-1]:
+            o[-1]+=1
+        else:
+            c.append(s[k])
+            o.append(1)
 
-    return [ ]
+    return list(zip(c, o))
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères passée
+    en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +39,19 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
     # cas de base
+    if not s:
+        return []
     # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
+    i = 1
+    while i < len(s) and s[0] == s[i]:
+        i+=1
+    return [(s[0], i)] + artcode_r(s[i:])
 
 #### Fonction principale
 
-
 def main():
+    """Appel aux fonctions secondaires pour vérifier leur bon fonctionnement"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
